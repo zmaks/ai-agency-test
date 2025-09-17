c681d4a324cbacb8099b55a8d34ddba6d6180b20
Maksim.Zheltoukhov <maksim.zheltoukhov@jetbrains.com>
2025-09-17 10:48:38 +0200

    fix

--- src/main/kotlin/com/example/LlmJsonService.kt
+++ src/main/kotlin/com/example/LlmJsonService.kt
@@ -1,7 +1,6 @@
 package com.example
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.stereotype.Service
 
